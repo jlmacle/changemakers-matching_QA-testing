b472f711b276c49410c05ef0badfd96da7d5f9af
Jean-Louis <you@example.com>
2025-02-03 20:27:34 +0100

    Not sure if cloning the repository will lead to replicate the success. Found to generate an allure report from the existing spec.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,31 +1,21 @@
 import { defineConfig } from "cypress";
-import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
-import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
-import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import { allureCypress } from "allure-cypress/reporter";
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://127.0.0.1:3000',
-    // https://docs.cypress.io/guides/references/configuration
-    experimentalStudio: true,
-    specPattern: "**/*.feature",
-    async setupNodeEvents(
-      on: Cypress.PluginEvents,
-      config: Cypress.PluginConfigOptions
-    ): Promise<Cypress.PluginConfigOptions> {
-      await addCucumberPreprocessorPlugin(on, config);
-      on(
-        "file:preprocessor",
-        createBundler({
-          plugins: [createEsbuildPlugin(config)],
-        })
-      );
+    specPattern: "**/*cy.js",
+    async setupNodeEvents(on, config) {
+      allureCypress(on, config);     
       return config;
     },
     env: {
       omitFiltered: true,
-      filterSpecs: true
+      filterSpecs: true,
+      allureReuseAfterSpec: false,
+      allure: true,
+      allureLogCypress: true
     },
-    fixturesFolder: false
-  },
+    fixturesFolder: false,
+  }
 });
