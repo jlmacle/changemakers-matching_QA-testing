001f2a4ba4037b64066ec960cc57014bea7269a1
Jean-Louis <you@example.com>
2025-02-13 10:11:15 +0100

    Added to spec

--- cypress/e2e/authentication-features/username-invalid.feature
+++ cypress/e2e/authentication-features/username-invalid.feature
@@ -6,4 +6,9 @@ Feature: Invalid username validation (in progress)
     When the user enters "Invalid_user_name..123" in the username field    
     Then the username validation message should display "⚠️ Invalid character present."
 
-  
+  Scenario: The user enters a username longer than 20 characters 
+  (cybersecurity limitations on how much data can be pushed to the system)
+
+    Given I am on the new account project representative page
+    When the user enters "Too_long_user_name_123456789" in the username field    
+    Then the username validation message should display "⚠️ The username is too long."
--- cypress/e2e/authentication-features/username-invalid.ts
+++ cypress/e2e/authentication-features/username-invalid.ts
@@ -4,11 +4,21 @@ Given('I am on the new account project representative page', () => {
   cy.visit('/www/_html/new-accountProject-representative.html');
 });
 
+// Username with .
 When('the user enters "Invalid_user_name..123" in the username field', () => {
   cy.get('#username').type('invaliduser.123');
 });
 
 Then('the username validation message should display "⚠️ Invalid character present."', () => {
   cy.get('#newAccount-projRep-errorInUsername').should('contain', '⚠️ Invalid character present.');
+});
+
+// Too long user name
+When('the user enters "Too_long_user_name_123456789" in the username field', () => {
+  cy.get('#username').type('Too_long_user_name_123456789');
+});
+
+Then('the username validation message should display "⚠️ The username is too long."', () => {
+  cy.get('#newAccount-projRep-errorInUsername').should('contain', '⚠️ The username is too long.');
 });
 
