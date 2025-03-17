fff43c43f5d29ff4ff06326235a38c5a6f37533c
Jean-Louis <you@example.com>
2025-03-17 10:24:10 +0100

    Added to the script to troubleshoot a reporting issue

--- report-generation_cypress.zsh
+++ report-generation_cypress.zsh
@@ -1,5 +1,7 @@
 # true added to pursue the script in case no folder would exist
+# Folders removed to mitigate issues with the report generation
 rm -r allure-results || true \
+rm -r allure-report || true \
 && npx cypress run \
 && npx allure generate --clean \
 && npx allure serve -h 127.0.0.1
