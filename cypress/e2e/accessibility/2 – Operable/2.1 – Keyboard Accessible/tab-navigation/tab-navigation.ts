083919e7ac3cd295f810a88e8598d82d4cd6f32e
Jean-Louis <you@example.com>
2025-04-04 18:09:12 +0200

    Spec updated to avoid flakiness

--- cypress/e2e/accessibility/2 – Operable/2.1 – Keyboard Accessible/tab-navigation/tab-navigation.ts
+++ cypress/e2e/accessibility/2 – Operable/2.1 – Keyboard Accessible/tab-navigation/tab-navigation.ts
@@ -5,6 +5,7 @@ import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
 Given('I am on the home page', () => {
   cy.visit('/www/index.html');
+  cy.get('body').should('be.visible');
 });
 
 // Common to the different tests
@@ -14,33 +15,45 @@ When('I press the Tab key to navigate to the next focusable element', () => {
 
 //1. Tabbing toward the "Skip To New Content" feature
 Then('I should arrive at the "Skip To New Content" feature', () => {
-  cy.focused().should('have.attr', 'id', "skipToMainContent");
+  cy.focused()
+  .should('be.visible')
+  .should('have.attr', 'id', "skipToMainContent");
 });
 
 
 //2. Tabbing toward "new account or login" button
 Then('I should arrive at the "new account or login" button', () => {
-  cy.focused().should('have.attr', 'id', "newAccountOrLogin-button");
+  cy.focused()
+  .should('be.visible')
+  .should('have.attr', 'id', "newAccountOrLogin-button");
 });
 
 //3. Tabbing toward title
 Then('I should arrive at the page title - home page', () => {
-  cy.focused().should('have.attr', 'id', "homePageTitle-link");
+  cy.focused()
+  .should('be.visible')
+  .should('have.attr', 'id', "homePageTitle-link");
 });
 
 //4. Tabbing toward testimonies
 Then('I should arrive at the testimonies', () => {
-  cy.focused().should('have.attr', 'id', "testimonies");
+  cy.focused()
+  .should('be.visible')
+  .should('have.attr', 'id', "testimonies");
 });
 
 //5. Tabbing toward about link
 Then('I should arrive at the "About" link', () => {
-  cy.focused().should('have.attr', 'id', "about-link");
+  cy.focused()
+  .should('be.visible')
+  .should('have.attr', 'id', "about-link");
 });
 
 //6. Tabbing toward about privacy link
 Then('I should arrive at the "Privacy" link', () => {
-  cy.focused().should('have.attr', 'id', "privacy-link");
+  cy.focused()
+  .should('be.visible')
+  .should('have.attr', 'id', "privacy-link");
 });
 
 
@@ -53,29 +66,40 @@ Then('I should arrive at the "Privacy" link', () => {
 
 Given('I am on the representative page', () => {
   cy.visit('/www/_html/new-accountProject-representative.html');
+  cy.get('body').should('be.visible');
 });
 
 //1. Tabbing toward title
 Then('I should arrive at the page title - representative page', () => {
-  cy.focused().should('have.attr', 'id', "homePageTitle-link");
+  cy.focused()
+  .should('be.visible')
+  .should('have.attr', 'id', "homePageTitle-link");
 });
 
 //2. Tabbing toward username field
 Then('I should arrive at the username field', () => {
-  cy.focused().should('have.attr', 'id', "username");
+  cy.focused()
+  .should('be.visible')
+  .should('have.attr', 'id', "username");
 });
 
 //3. Tabbing toward password field
 Then('I should arrive at the password field', () => {
-  cy.focused().should('have.attr', 'id', "password");
+  cy.focused()
+  .should('be.visible')
+  .should('have.attr', 'id', "password");
 });
 
 //4. Tabbing toward about link
 Then('I should arrive at the "About" link - representative page', () => {
-  cy.focused().should('have.attr', 'id', "about-link");
+  cy.focused()
+  .should('be.visible')
+  .should('have.attr', 'id', "about-link");
 });
 
 //5. Tabbing toward about privacy link
 Then('I should arrive at the "Privacy" link - representative page', () => {
-  cy.focused().should('have.attr', 'id', "privacy-link");
+  cy.focused()
+  .should('be.visible')
+  .should('have.attr', 'id', "privacy-link");
 });
