9a4cd43e57044bd882779a42995de39f4e197d3d
Jean-Louis <you@example.com>
2025-04-13 08:27:31 +0200

    Code cleanup

--- cypress/e2e/accessibility/3. Understandable/3.3 Input Assistance/3.3.1 Error Identification Level A/invalid-username-équipe-displays-username-constraints.ts
+++ cypress/e2e/accessibility/3. Understandable/3.3 Input Assistance/3.3.1 Error Identification Level A/invalid-username-équipe-displays-username-constraints.ts
@@ -2,7 +2,7 @@ import { When } from '@badeball/cypress-cucumber-preprocessor';
 
 // Scenario: invalid username "parentalité"
 
-When('The user enters "parentalité" in the username field', () => {
+When('The user enters "équipe" in the username field', () => {
   cy.get('#username')
   .should('be.visible')
   .clear()
