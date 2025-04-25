015f139e717e38849cf4295d8eab21edb492e8db
Jean-Louis <you@example.com>
2025-04-25 10:32:06 +0200

    Code cleanup

--- z-Allure-report-generation-and-display.zsh
+++ z-Allure-report-generation-and-display.zsh
@@ -1,8 +1,8 @@
 # true added to pursue the script in case no folder would exist,
 # or in case some tests would fail
 # Folders removed to mitigate issues with the report generation
-rm -r allure-results || true \
-rm -r allure-report || true \
+rm -r z-allure-results || true \
+rm -r z-allure-report || true \
 && npx cypress run || true\
-&& npx allure generate --clean \
-&& npx allure serve -h 127.0.0.1
+&& npx allure generate ./z-allure-results --clean -o ./z-allure-report \
+&& npx allure open ./z-allure-report
