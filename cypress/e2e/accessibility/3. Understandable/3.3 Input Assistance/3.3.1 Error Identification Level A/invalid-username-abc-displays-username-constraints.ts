dc6a9c49683bc552b404a6c90a6db9c68835d162
Jean-Louis <you@example.com>
2025-05-05 15:36:21 +0200

    Code cleanup

--- cypress/e2e/accessibility/3. Understandable/3.3 Input Assistance/3.3.1 Error Identification Level A/invalid-username-abc-displays-username-constraints.ts
+++ cypress/e2e/accessibility/3. Understandable/3.3 Input Assistance/3.3.1 Error Identification Level A/invalid-username-abc-displays-username-constraints.ts
@@ -3,8 +3,5 @@ import { When } from '@badeball/cypress-cucumber-preprocessor';
 // Scenario: invalid username "abc"
 
 When('the user enters "abc" in the username field', () => {
-  cy.get('#username')
-  .should('be.visible')
-  .clear()
-  .type("abc");
+  cy.typeInUsername("abc");
 });
--- cypress/e2e/accessibility/3. Understandable/3.3 Input Assistance/3.3.1 Error Identification Level A/valid-username-Valid_123-displays-validity-message.ts
+++ cypress/e2e/accessibility/3. Understandable/3.3 Input Assistance/3.3.1 Error Identification Level A/valid-username-Valid_123-displays-validity-message.ts
@@ -3,10 +3,7 @@ import { When } from '@badeball/cypress-cucumber-preprocessor';
   // Scenario: valid username "Valid_123"
  
 	When('the user enters "Valid_123" in the username field', () => {
-    cy.get('#username')
-    .should('be.visible')
-    .clear()
-    .type('Valid_123');
+    cy.typeInUsername("Valid_123");
   });
 
   
