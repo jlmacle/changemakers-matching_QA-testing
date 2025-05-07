5efcf554574bded4720035c3911497a1b2eaf92c
Jean-Louis <you@example.com>
2025-05-07 07:52:36 +0200

    Notes cleanup

--- cypress/e2e/accessibility/_Common steps/username-validation-common-steps.ts
+++ cypress/e2e/accessibility/_Common steps/username-validation-common-steps.ts
@@ -1,20 +1,13 @@
 import { Then } from '@badeball/cypress-cucumber-preprocessor';
 
-Then('the username validation message should display "⚠️ The username must between 4 and 10 characters long."', () => {
-    cy.get('#newAccount-projRep-errorInUsername')
-    .should('be.visible')
-    .should('contain', '⚠️ The username must between 4 and 10 characters long.');  
-  }); 
-
+  Then('the username validation message should display "⚠️ The username must between 4 and 10 characters long."', () => {
+    cy.assertErrorInUsername('⚠️ The username must between 4 and 10 characters long.');  
+  });
+  
   Then('the username validation message should display', (docString: string) => {
-    cy.get('#newAccount-projRep-errorInUsername')
-    .should('be.visible')
-    .should('contain', "⚠️ Invalid character present.")
-    .should('contain', "The username can only contain lowercase letters (without accents), numbers and underscores.")
+    cy.assertErrorInUsername("The username can only contain lowercase letters (without accents), numbers and underscores.");
   }); 
 
   Then('the username validation message should display "✅ The username is valid."', () => {
-    cy.get('#newAccount-projRep-errorInUsername')
-    .should('be.visible')
-    .should('contain', '✅ The username is valid.');
+    cy.assertErrorInUsername('✅ The username is valid.');
   });
