f76a63a826b4313429ab16ecd15e1aa449ecbd9c
Jean-Louis <you@example.com>
2025-05-16 19:24:36 +0200

    Code cleanup

--- c-playwright-tests/Accessibility/2. Operable/2.1 Keyboard Accessible/Tab-navigation/tab-navigation-home-page.spec.ts
+++ c-playwright-tests/Accessibility/2. Operable/2.1 Keyboard Accessible/Tab-navigation/tab-navigation-home-page.spec.ts
@@ -1,26 +1,23 @@
-import { test, expect } from '@playwright/test';  
+import { test, expect } from '@playwright/test';    
   
-  test.afterEach(async ({ page }) => {
-    await page.close();
-  });
-
+  // new browser context for each test  
+  // https://playwright.dev/docs/browser-contexts#introduction
   test('Tab Navigation on the Home Page', async ({ page }) => {
-  await page.goto('/www/index.html', { waitUntil: 'load' });
+    await page.goto('/www/index.html', { waitUntil: 'load' });
 
-  // The sequence of data-test values we expect to receive focus
-  const dataTests = [
-    'skipToMainContent',
-    'newAccountOrLogin-button',
-    'title-link',
-    'testimonies',
-    'about-link',
-    'privacy-link'
-  ];
+    // The sequence of data-test values we expect to receive focus
+    const dataTests = [
+      'skipToMainContent',
+      'newAccountOrLogin-button',
+      'title-link',
+      'testimonies',
+      'about-link',
+      'privacy-link'
+    ];
 
-  for (const dataTest of dataTests) {
-    await page.keyboard.press('Tab');
-
-    const focused = page.locator(':focus-visible');
-    await expect(focused).toHaveAttribute('data-test', dataTest);
-  }
+    for (const dataTest of dataTests) {
+      await page.keyboard.press('Tab');
+      const focused = page.locator(':focus-visible');
+      await expect(focused).toHaveAttribute('data-test', dataTest);
+    }
 });
