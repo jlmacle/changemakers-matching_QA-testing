c86e7cea8546f2d5e709aaa4245f70a032d74a6f
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2025-06-03 09:54:16 +0200

    Externalized the sharding

--- z-Playwright-report-generation-and-display-With-sharding-Seems-more-robust-to-failed-tests-and-flaky-tests.zsh
+++ z-Playwright-report-generation-and-display-With-sharding-Seems-more-robust-to-failed-tests-and-flaky-tests.zsh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
-# Augmenting the number of shards when the tests are flaky seems to be effective
-TOTAL_SHARDS=6
-
-# Running each shard, emiting blob, never auto-opening HTML
-for ((i=1; i<=TOTAL_SHARDS; i++)); do
-  echo "Running shard ${i}/${TOTAL_SHARDS}"
-  PLAYWRIGHT_HTML_OPEN=never \
-    npx playwright test \
-      --shard=${i}/${TOTAL_SHARDS} \
-      --reporter=blob
-    mv ./blob-report/report-${i}.zip  ./blob-reports-tmp/|| true
-    sleep 5
-done
+#Sharding code
+cd _Zsh_utility-scripts/
+./playwright-sharding.zsh
+cd ..
 
 # Merging all blob reports into a single HTML report
 echo "Merging reports"
