17596b9c769c7126d3da16e90717f6d335ae3753
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2025-06-12 08:24:05 +0200

    Code cleanup

--- cypress/e2e/accessibility/_Common steps/presence-on-a-page-common-steps.ts
+++ cypress/e2e/accessibility/_Common steps/presence-on-a-page-common-steps.ts
@@ -3,5 +3,4 @@ import { Given } from '@badeball/cypress-cucumber-preprocessor';
 Given('I am on the new account project representative page', () => {
   cy.visit('/www/_html/new-accountProject-representative.html');
   cy.get('body').should('be.visible');
-  cy.get('[data-test="username"]').should('be.visible');
 });
