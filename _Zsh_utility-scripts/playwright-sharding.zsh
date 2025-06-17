446a6e3bb136e2af19b2edd498009b183d4b73a4
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2025-06-17 16:08:54 +0200

    Code cleanup

--- _Zsh_utility-scripts/playwright-sharding.zsh
+++ _Zsh_utility-scripts/playwright-sharding.zsh
@@ -1,15 +1,22 @@
+ARG=$1
+echo "Running Playwright tests with arguments: ${ARG}"
+
+# Removing previous reports if any
+rm blob-report/*.zip 2>/dev/null || true
+rm blob-reports-tmp/*.* 2>/dev/null || true
+
 # Augmenting the number of shards when the tests are flaky seems to be effective
 TOTAL_SHARDS=6
 
-
-pwd
+cd ..
 # Running each shard, emiting blob, never auto-opening HTML
 for ((i=1; i<=TOTAL_SHARDS; i++)); do
   echo "Running shard ${i}/${TOTAL_SHARDS}"
   PLAYWRIGHT_HTML_OPEN=never \
-    npx playwright test "c-playwright-tests/User-Accessibility-SMS-104/2. Operable/2.1 Keyboard Accessible/Tab-navigation" "c-playwright-tests/User-Accessibility-SMS-104/3. Understandable/3.3 Input Assistance/3.3.1 Error Identification Level A" \
+    npx playwright test ${ARG} \
       --shard=${i}/${TOTAL_SHARDS} \
       --reporter=blob
-    mv ./blob-report/report-${i}.zip  ./blob-reports-tmp/|| true
+    
+    mv ./blob-report/report-*-${i}.zip ./blob-reports-tmp/|| true
     sleep 5
 done
--- zz-comparison-cypress-playwright.py
+++ zz-comparison-cypress-playwright.py
@@ -12,6 +12,6 @@ def time_script(name, cmd):
 # Testing on the specs related to the username validity
 d1 = time_script("Cypress: npx cypress run", "npx cypress run --spec 'cypress/e2e/User-Accessibility-SMS-104/3. Understandable/3.3 Input Assistance/3.3.1 Error Identification Level A'")
 time.sleep(5)
-d2 = time_script("Playwright with sharding", "./_Zsh_utility-scripts/playwright-sharding.zsh")
+d2 = time_script("Playwright with sharding", "./_Zsh_utility-scripts/playwright-sharding.zsh username")
 
 print(f"Cypress: npx cypress run: {d1:.4f} s | Playwright with sharding: {d2:.4f} s \n")
