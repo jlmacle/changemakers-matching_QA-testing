1359cb0469364e4f618e836889f08834651d4d30
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2025-07-14 10:21:04 +0200

    Code cleanup

--- _Zsh_utility-scripts/playwright-sharding.zsh
+++ _Zsh_utility-scripts/playwright-sharding.zsh
@@ -1,16 +1,16 @@
-PARAMETER=$1
+SPECIFIC_TESTS_TO_RUN=$1 # Passed in parameter to the script
 BLOB_DIR="./blob-report"
 AGGREGATED_BLOBS_DIR="./blob-reports-tmp"
 
-echo "Playwright tests to be run with parameter: ${PARAMETER}"
+echo "Playwright tests to be run with parameter: ${SPECIFIC_TESTS_TO_RUN}"
 
-PROJECT_PATH=$(git rev-parse --show-toplevel) # Importance of having no spaces around the equal sign
+PROJECT_PATH=$(git rev-parse --show-toplevel) 
 echo "Project root directory: $PROJECT_PATH"
 cd $PROJECT_PATH # to have the right folder to start the Playwright tests from, independently from where the script is run
 
 # Removing previous reports if any
-rm "$BLOB_DIR/*.*" 2>/dev/null || true
-rm "$AGGREGATED_BLOBS_DIR/*.*" 2>/dev/null || true
+rm "$BLOB_DIR/*.*" 2>/dev/null 
+rm "$AGGREGATED_BLOBS_DIR/*.*" 2>/dev/null 
 
 # Augmenting the number of shards when the tests are flaky seems to be effective
 TOTAL_SHARDS=9
@@ -18,11 +18,11 @@ TOTAL_SHARDS=9
 # Running each shard, creating the data blob
 for ((i=1; i<=TOTAL_SHARDS; i++)); do
   echo "Running shard ${i}/${TOTAL_SHARDS}"
-  npx playwright test ${PARAMETER} \
+  npx playwright test ${SPECIFIC_TESTS_TO_RUN} \
     --shard=${i}/${TOTAL_SHARDS} \
     --reporter=blob
   
-  mv "$BLOB_DIR/report-*-${i}.zip" "$AGGREGATED_BLOBS_DIR" 2>/dev/null|| true
-  mv "$BLOB_DIR/report-${i}.zip" "$AGGREGATED_BLOBS_DIR" 2>/dev/null|| true
+  mv "$BLOB_DIR/report-*-${i}.zip" "$AGGREGATED_BLOBS_DIR" 2>/dev/null
+  mv "$BLOB_DIR/report-${i}.zip" "$AGGREGATED_BLOBS_DIR" 2>/dev/null
   sleep 5 # Giving time for the computer to recover from the procesing load
 done
