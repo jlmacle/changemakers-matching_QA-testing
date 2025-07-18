7597c9ef6dba06b26634c10de67968ade02e3f72
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2025-07-18 08:56:38 +0200

    Code cleanup

--- z-Playwright-report-generation-and-display-With-sharding-Seems-more-robust-to-failed-tests-and-flaky-tests.zsh
+++ z-Playwright-report-generation-and-display-With-sharding-Seems-more-robust-to-failed-tests-and-flaky-tests.zsh
@@ -5,19 +5,19 @@ AGGREGATED_BLOBS_DIR="./blob-reports-tmp"
 
 # Removing the previous report if any
 echo "Removing the previous report if any"
-rm -r "$REPORT_DIR" 2>/dev/null
+rm -r $REPORT_DIR 2>/dev/null
 
 # Code sharding
 echo "Running the tests" 
-cd "$SHARDING_SCRIPT_DIR"
+cd $SHARDING_SCRIPT_DIR
 ./playwright-sharding.zsh
 cd ..
 
 # Merging all blob reports into a single HTML report
 echo "Merging reports"
-npx playwright merge-reports --reporter=html "$AGGREGATED_BLOBS_DIR"
+npx playwright merge-reports --reporter=html $AGGREGATED_BLOBS_DIR
 
 # Opening the merged report in the browser
-mv playwright-report "$REPORT_DIR" #playwright-report created by default
+mv playwright-report $REPORT_DIR #playwright-report created by default
 echo "Opening merged report"
-npx playwright show-report "$REPORT_DIR"
+npx playwright show-report $REPORT_DIR
