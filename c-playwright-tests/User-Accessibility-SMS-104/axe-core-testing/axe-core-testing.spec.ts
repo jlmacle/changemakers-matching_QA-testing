eec36c63cd79176b04b1dae85316c7e12f2d43b4
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2025-07-21 11:55:44 +0200

    Code cleanup

--- c-playwright-tests/User-Accessibility-SMS-104/axe-core-testing/axe-core-testing.spec.ts
+++ c-playwright-tests/User-Accessibility-SMS-104/axe-core-testing/axe-core-testing.spec.ts
@@ -18,5 +18,21 @@ test.describe('Pages Accessibility', () => {
     
   });
 
+  test('The representative page should have no critical accessibility violations', async ({ page }) => {
+    await page.goto('/www/_html/new-accountProject-representative.html', { waitUntil: 'load' });
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(['wcag2a', 'wcag2aa']) 
+      .analyze();
+    
+    // Log violations for easier debugging, even if the test passes based on filtered criteria
+    if (accessibilityScanResults.violations.length > 0) {
+      console.log('Axe-core: Accessibility Violations Found:');
+      console.log(accessibilityScanResults.violations);
+      expect(accessibilityScanResults.violations).toEqual([]);
+    }
+    else console.log('Axe-core: No accessibility violations found.');
+    
+  });
+
   
 });
